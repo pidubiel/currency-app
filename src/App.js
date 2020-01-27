14e8eeecc9238db751e9bccfc17ba92e850d5639
Piotr Dubiel <piotrdubiel@icloud.com>
2020-01-27 18:35:05 +0100

    Added basic styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles/main.scss';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App main-container'>
       <header className='App-header'>Currency app</header>
     </div>
   );
